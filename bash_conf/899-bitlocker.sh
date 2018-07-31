001d6fb8471abd6d371e12f71f56b071fcbba0d8
Candido Tominaga <candido1212@gmail.com>
2018-07-31 20:56:45 -0300

    bitlocker Mount

---
+++ bash_conf/899-bitlocker.sh
@@ -0,0 +1,25 @@
+# Bitlocker para Linux (read/write)
+# https://superuser.com/questions/376533/how-to-access-a-bitlocker-encrypted-drive-in-linux
+
+ct_bitlockerInstall() {
+    sudo apt-get install dislocker
+}
+
+[[ -f /usr/bin/dislocker ]] || { return ; }
+
+
+ct_bitlockerMount() {
+    local DEV_SDAx="$1"
+    local CHAVE="$2"
+    local PONTO_DE_MONTAGEM="$3"
+
+    sudo mkdir -p "$PONTO_DE_MONTAGEM"
+    
+    local BL_FOLDER=`echo $DEV_SDAx | sed "s@/dev/@@"`
+    sudo mkdir /mnt/$BL_FOLDER
+    sudo dislocker -v -V "$DEV_SDAx" -p$CHAVE -- /mnt/$BL_FOLDER
+
+    sudo mount -o loop /mnt/$BL_FOLDER/dislocker-file "$PONTO_DE_MONTAGEM"
+
+    export DISLOCKER_UMOUNT_$BL_FOLDER="sudo umount $PONTO_DE_MONTAGEM && sudo umount $DEV_SDAx"
+}
