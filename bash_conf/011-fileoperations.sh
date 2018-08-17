0cc45771ae6ccefc2387c40ecbd98d89c294f185
Candido Tominaga <candido.tominaga@copel.com>
2018-08-17 13:55:19 -0300

    ct_findAndRemoveFilesOlderDays

--- bash_conf/011-fileoperations.sh
+++ bash_conf/011-fileoperations.sh
@@ -11,6 +11,14 @@ ct_findAndRemoveFiles() {
     find . -type f -name $tmp -exec rm -f {} \;
 }
 
+ct_findAndRemoveFilesOlderDays() {
+    local EM_QUAL_PASTA="$1"
+    local MAIS_QUE_DIAS=$2
+    find "$EM_QUAL_PASTA" -mindepth 1 -mtime +$MAIS_QUE_DIAS -delete
+}
+
+
+
 ct_grepInFileExtension() {
     local tmp="'"$1"'"
     grep -ir $tmp --include \*.$2
