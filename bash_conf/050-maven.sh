0cecd28cb62bea0628aaa2e7b83eab574ffb8027
Candido Tominaga <c049262@copel.nt>
2019-02-21 13:42:51 -0300

    removido mvninstall

--- bash_conf/050-maven.sh
+++ bash_conf/050-maven.sh
@@ -8,17 +8,17 @@
 export MVN=mvn
 
 MVN_SKIP_TESTS="-DskipTests=true"
+MVN_WTPVERSION="-Dwtpversion=2.0"
 
 alias mvnclean="$MVN clean"
-alias mvninstall="$MVN install $MVN_SKIP_TESTS"
-alias mvnreset="mvnclean; mvneclipseclean; mvninstall; mvneclipseeclipse; mvnpiceclipse"
-alias mvncleaninstall="$MVN clean install $MVN_SKIP_TESTS"
-alias mvneclipseeclipse="$MVN eclipse:eclipse -Dwtpversion=2.0"
+alias mvnpackage="$MVN install $MVN_SKIP_TESTS"
+alias mvnreset="mvnclean; mvneclipseclean; mvnpackage; mvneclipseeclipse; mvnpiceclipse"
+alias mvncleanpackage="$MVN clean package $MVN_SKIP_TESTS"
+alias mvneclipseeclipse="$MVN eclipse:eclipse $MVN_WTPVERSION"
 alias mvnpiceclipse="$MVN picEclipse:corrige"
 alias mvneclipseclean="$MVN eclipse:clean; rm -fr target bin"
 alias mvnDeploy="$MVN deploy $MVN_SKIP_TESTS"
 alias mvnVersion="$MVN -v"
-alias mvnpackage="$MVN clean package $MVN_SKIP_TESTS"
 
 alias mvnDependencyTree="$MVN dependency:tree"
 alias mvnDependencyList="$MVN dependency:list"
