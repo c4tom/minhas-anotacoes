1775dc633463b91bd1d4b2d5ccadfc7b3a9a67a2
Candido H Tominaga <c049262@copel.nt>
2019-05-20 16:12:12 -0300

    nodejsCorrecoesProblemas

--- bash_conf/060-npm-nodejs.sh
+++ bash_conf/060-npm-nodejs.sh
@@ -12,3 +12,19 @@ nodejsInstallLinuxMint() {
 # Tools
 # https://www.npmjs.com/package/sitemap-generator-cli
 #	
+
+
+nodejsCorrecoesProblemas() {
+	
+	#npm ERR! code UNABLE_TO_GET_ISSUER_CERT_LOCALLY
+	#npm ERR! errno UNABLE_TO_GET_ISSUER_CERT_LOCALLY
+	#npm ERR! request to https://registry.npmjs.org/mime-db/-/mime-db-1.40.0.tgz failed, reason: unable to get local issuer certificate
+	npm config set registry http://registry.npmjs.org/
+
+	#Error: unable to get local issuer certificate
+	#Error: unable to get local issuer certificate
+    #  at TLSSocket.onConnectSecure (_tls_wrap.js:1177:34)
+    #  at TLSSocket.emit (events.js:193:13)
+    #  at TLSSocket._finishInit (_tls_wrap.js:668:8)
+	npm config set strict-ssl false
+}
