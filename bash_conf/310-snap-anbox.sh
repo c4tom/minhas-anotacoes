db2c69f79d04ddbcb883511a9a0e12c2a14fda5a
Candido Tominaga <candido1212@gmail.com>
2019-07-09 11:17:52 -0300

    ct_anboxBridge

--- bash_conf/310-snap-anbox.sh
+++ bash_conf/310-snap-anbox.sh
@@ -4,6 +4,8 @@
 
 # https://github.com/anbox/anbox/blob/master/docs/install.md
 # https://docs.anbox.io/userguide/install.html
+# https://linuxconfig.org/how-to-install-anbox-and-run-android-apps-in-linux
+
 ct_anboxInstall() {
     sudo add-apt-repository ppa:morphis/anbox-support
     sudo apt update
@@ -18,6 +20,16 @@ ct_anboxInstall() {
     sudo snap install --devmode --beta anbox
 
     snap info anbox
+}
+
+
+ct_anboxRestartService() {
+    sudo systemctl restart snap.anbox.container-manager.service
+}
+
+#https://github.com/Debyzulkarnain/anbox-bridge/blob/master/anbox-bridge.sh
+ct_anboxBridge() {
+    sudo /usr/bin/anbox-bridge.sh start
 }
 
 ct_anboxUninstall(){
