d95d39cf39bd9e23034aea9c972b8bce742af55d
Candido Tominaga <candido1212@gmail.com>
2019-08-23 10:57:24 -0300

    :/desenv/java/apache-cxf-3.3.3/bin

--- bash_conf/000-bashrc.sh
+++ bash_conf/000-bashrc.sh
@@ -1,4 +1,5 @@
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/desenv/java/apache-cxf-3.3.3/bin
 
 #[[ $(id -u) -eq 0 ]] || { echo >&2 "Must be root to run this function"; return; }
 
+alias gksu='pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY'
