1f711ac200e0ab769bbbf743c3c1a730c1b02f2e
Candido Tominaga <candido1212@gmail.com>
2019-08-26 16:30:24 -0300

    ct_snapInstallAndroidStudio ct_snapInstallAllDesenv

--- bash_conf/309-snapd.sh
+++ bash_conf/309-snapd.sh
@@ -7,3 +7,24 @@
 ct_snapdInstall() {
     sudo apt-get install snapd
 }
+
+ct_snapInstallAndroidStudio() {
+    sudo apt-get install qemu-kvm
+
+    sudo snap install android-studio --classic
+}
+
+ct_snapInstallAllDesenv() {
+    local CLASSIC="code code-insiders eclipse intellij-idea-community intellij-idea-ultimate netbeans node phpstorm powershell pycharm-educational rider "
+    local EDGE="dbeaver-ce"
+
+    for i in $CLASSIC
+        do
+            sudo snap install $i --classic 
+        done
+    
+    for i in $EDGE
+        do 
+            sudo snap install $i --edge
+        done
+}
