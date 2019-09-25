188624154f4f93df355b2c11379fb7d6cfb2d79d
Candido Tominaga <candido1212@gmail.com>
2019-09-25 08:23:04 -0300

    jdk12() jdk8() jre8()

--- bash_conf/040-java.sh
+++ bash_conf/040-java.sh
@@ -1,40 +1,26 @@
 # Baixar arquivos antigos: http://www.oracle.com/technetwork/java/javase/archive-139210.html
 export CXF_HOME=/desenv/java/apache-cxf-3.3.3
+export JAXWS_HOME=/desenv/java/jaxws-ri
 
+JDK_HOME="/desenv/java/jdk8"
+JAVA_HOME=$JDK_HOME
 
-JDK_HOME="/desenv/jdk/jdk8"
-JAVA_HOME=$JDK_HOME;
-
-jdk8()
-{
-	JAVA_HOME=/desenv/jdk/jdk8
-	JDK_HOME=/desenv/jdk/jdk8
+jdk12() {
+	JAVA_HOME=/desenv/java/jdk12
+	JDK_HOME=$JAVA_HOME
+	export PATH=$PATH:$JDK_HOME/bin
 }
 
-jdk7()
-{
-	JAVA_HOME=/desenv/jdk/jdk7
-	JDK_HOME=/desenv/jdk/jdk7
+jdk8() {
+	JAVA_HOME=/desenv/java/jdk8
+	JDK_HOME=$JAVA_HOME
+	export PATH=$PATH:$JDK_HOME/bin
 }
 
-jdk6()
-{
-	JAVA_HOME=/desenv/jdk/jdk6
-	JDK_HOME=/desenv/jdk/jdk6
+jre8() {
+	JAVA_HOME=/desenv/java/jdk8/jre
+	JDK_HOME=/desenv/java/jdk8
 }
-
-
-jre8()
-{
-	JAVA_HOME=/home/desenv/jre/jre8
-	JDK_HOME=/home/desenv/jre/jre8
-}
-jre7()
-{
-	JAVA_HOME=/home/desenv/jre/jre7
-	JDK_HOME=/home/desenv/jre/jre7
-}
-
 
 # Lista todas bibliotecas que tem dentro de um arquivo EAR.
 # Obs.: necessario instalar apt-get install jar
@@ -43,25 +29,22 @@ javaLibsIntoEAR() {
 	jar -tvf $1
 }
 
-javaShowManifest(){
-	local file=$(realpath $1);
+javaShowManifest() {
+	local file=$(realpath $1)
 	unzip -q -c $file META-INF/MANIFEST.MF
 }
 
-
-javaDecompiler()
-{
-	java -jar /desenv/java/jd-gui-1.4.0.jar "$1"
+# http://java-decompiler.github.io/
+javaDecompiler() {
+	java -jar /desenv/java/decomp/jd-gui-1.6.3.jar "$1"
 }
-
 
 # Procura bibliotecas JARS que esta esteja no sistema/no computador na pasta contendo EAP
 procuraJarsNoSistema() {
 	# $1 - arquivo .war
-	for i in $(unzip -l "$1" | grep WEB-INF/lib | awk -F"/" '{print $3}')
-	do
+	for i in $(unzip -l "$1" | grep WEB-INF/lib | awk -F"/" '{print $3}'); do
 		echo -e $CRed$i$NC
-		locate $i | grep "$i\$" | grep eap;
+		locate $i | grep "$i\$" | grep eap
 	done
 
 }
@@ -74,3 +57,5 @@ ct_java_PrintFlagsFinal() {
 ct_java_ListProcess() {
 	jps -v
 }
+
+export PATH=$PATH:$CXF_HOME/bin:$JAXWS_HOME/bin
