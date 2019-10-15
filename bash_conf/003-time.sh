2467b8d1b8e7e7cb8fcb43dd914b7ae1e7110912
Candido Tominaga <candido1212@gmail.com>
2019-10-14 23:58:29 -0300

    ct_datehoraAtualizaComNTP

--- bash_conf/003-time.sh
+++ bash_conf/003-time.sh
@@ -3,3 +3,7 @@
 ct_timeCorrigirLinuxEWindows() {
     xdg-open 'https://www.edivaldobrito.com.br/como-corrigir-diferencas-de-tempo-entre-o-ubuntu-e-o-windows-em-sistema-com-dual-boot/'
 }
+
+ct_datehoraAtualizaComNTP() {
+    sudo ntpdate 1.ro.pool.ntp.org
+}
