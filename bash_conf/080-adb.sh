a25fee28d23855fd2244719e7f2dbb145fc8fa98
Candido Tominaga <candido1212@gmail.com>
2019-11-09 21:48:20 -0300

    ct_adbListarTodosPacotesAPK  ct_adbListaPathPacotesAPK

--- bash_conf/080-adb.sh
+++ bash_conf/080-adb.sh
@@ -1,6 +1,20 @@
 [[ -f /usr/bin/adb ]] || { return ; }
 
+# https://www.automatetheplanet.com/adb-cheat-sheet/
+
 # https://www.technipages.com/how-to-backup-your-entire-android-device
 ct_adbBackupAll() {
     adb backup -all -f backup-android.ab
+}
+
+ct_adbListarTodosPacotesAPK() {
+    adb shell pm list packages | awk -F':' '{print $2}'
+}
+
+ct_adbListaPathPacotesAPK() {
+    for i in `ct_adbListarTodosPacotesAPK`
+    do
+        echo $i;
+        echo "adb shell pm path "$i" | awk -F':' '{print $2}'"
+    done    
 }
