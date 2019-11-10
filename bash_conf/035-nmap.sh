1a957c5d8f5a8321b6fe16e7433a2b64418a850d
Candido Tominaga <candido1212@gmail.com>
2019-11-09 21:48:50 -0300

    ct_nmapLiveHostOnNetwork

--- bash_conf/035-nmap.sh
+++ bash_conf/035-nmap.sh
@@ -8,3 +8,9 @@
 ct_nmapListaTodosSubDominios() {
 	nmap -script dns-brute -v -sn $1;
 }
+
+ct_nmapLiveHostOnNetwork() {
+    # 192.168.1.1/24
+    local IP_NETWORK="$1/24"
+    nmap $1 -n -sP | grep report | awk '{print $5}'
+}
