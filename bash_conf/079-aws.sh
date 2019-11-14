7cc22ee634cc86f9cac20f2f99ebdece313b342e
Candido Tominaga <candido1212@gmail.com>
2019-11-13 21:59:46 -0300

    ct_awsSetKeyFromCredentialFile

--- bash_conf/079-aws.sh
+++ bash_conf/079-aws.sh
@@ -1,5 +1,5 @@
 
-if [[ -f /usr/bin/aws ]] 
+if [[ ! -f /usr/bin/aws ]] 
 then
 	ct_awsInstall() {
 		sudo snap install aws-cli --classic
@@ -10,9 +10,9 @@ fi
 # https://docs.aws.amazon.com/pt_br/cli/latest/userguide/cli-configure-envvars.html
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
-export AWS_DEFAULT_REGION="us-west-2"
+export AWS_DEFAULT_REGION="us-west-1"
 
-ct_setKeys() {
+ct_awsSetKeys() {
     local KEY=$1
     local SEC=$2
     local REG=$3
@@ -24,6 +24,23 @@ ct_setKeys() {
     export AWS_DEFAULT_REGION="$3"
 }
 
+ct_awsSetKeyFromCredentialFile() {
+    if [ -z "$1" ] 
+        then
+            echo "Informe o nome do bucket: "
+            read AWS_BUCKET
+            export AWS_BUCKET
+        else
+            export AWS_BUCKET="$1";
+        fi
+
+    export AWS_SECRET_ACCESS_KEY=`ct_parseINI ~/.aws/credentials default aws_secret_access_key`
+    export AWS_ACCESS_KEY_ID=`ct_parseINI ~/.aws/credentials default aws_access_key_id`
+    export AWS_DEFAULT_REGION=`ct_parseINI ~/.aws/config default region`
+}
+
 ct_awsAutoComplete() {
     complete -C '/snap/aws-cli/151/bin/aws_completer' aws
 }
+
+
