0cd8a5760f56f63fd4c5da9f85132a9d39527b3f
Candido Tominaga <candido1212@gmail.com>
2020-05-20 15:52:26 -0300

    ct_youtubeDownloadLiveVideo

--- bash_conf/999-outros.sh
+++ bash_conf/999-outros.sh
@@ -3,3 +3,19 @@
 ct_whatsapp() {
     xdg-open https://web.whatsapp.com/send?phone=+55$1&text&source&data
 }
+
+
+
+# https://stackoverflow.com/questions/37040798/how-do-you-use-youtube-dl-to-download-live-streams-that-are-live
+# sudo add-apt-repository ppa:nilarimogard/webupd8
+# sudo apt update
+# sudo apt install streamlink
+ct_youtubeDownloadLiveVideo() {
+    local URL="$1"
+    local FILE_NAME="$2"
+    echo "Escolha a qualidade de gravação: "
+    streamlink ${URL}
+    read QUALITY
+
+    streamlink ${URL} ${QUALITY} -o ${FILE_NAME}
+}
