9735afc80e96ab51a9b2d977297def7da300dd4b
Candido Tominaga <candido1212@gmail.com>
2020-09-01 09:25:15 -0300

    ct_monitorar_pasta

---
+++ bash_conf/009-inotify.sh
@@ -0,0 +1,15 @@
+[[ -f /usr/bin/inotifywait ]] || {
+   ct_install_inotify-tools() {
+      sudo apt install inotify-tools
+   }
+   return;
+}
+
+# https://stackoverflow.com/questions/8699293/how-to-monitor-a-complete-directory-tree-for-changes-in-linux
+
+ct_monitorar_pasta() {
+   : ${1?' informe a pasta'}
+   while true; do
+      inotifywait -e modify,create,delete,move -r $1
+   done
+}
