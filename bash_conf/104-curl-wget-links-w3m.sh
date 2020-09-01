dd005331dc78990dd1b8fba9f933104a69fb9b54
Candido Tominaga <candido1212@gmail.com>
2020-09-01 09:22:23 -0300

    ct_curl_test_speed_site

---
+++ bash_conf/104-curl-wget-links-w3m.sh
@@ -0,0 +1,6 @@
+
+# https://www.tecmint.com/test-website-loading-speed-in-linux-terminal/
+ct_curl_test_speed_site() {
+   : ${1?: "<url>"}
+   curl -s -w 'Testing Website Response Time for: %{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nAppCon Time:\t\t%{time_appconnect}\nRedirect Time:\t\t%{time_redirect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null $1
+}
