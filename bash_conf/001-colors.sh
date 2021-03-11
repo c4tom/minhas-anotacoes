b331f0cedd65a474bd67cc06657415de468fa8a7
Candido Tominaga <candido1212@gmail.com>
2021-03-11 11:28:16 -0300

    echoAllcolorsMatrix

--- bash_conf/001-colors.sh
+++ bash_conf/001-colors.sh
@@ -97,3 +97,16 @@ echoWhiteBlack() {
 echoYellowBlack() {
 	echo -e "$CYellow$On_Black$1$Color_Off"
 }
+
+function echoAllcolorsMatrix() {
+    # credit to http://askubuntu.com/a/279014
+    for x in 0 1 4 5 7 8; do
+        for i in `seq 30 37`; do
+            for a in `seq 40 47`; do
+                echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+            done
+            echo
+        done
+    done
+    echo ""
+}
