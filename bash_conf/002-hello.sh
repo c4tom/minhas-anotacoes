ccbb6df47588c53a90549af40fb608b4fb16706d
Candido Tominaga <candido1212@gmail.com>
2021-03-11 11:28:29 -0300

    linux

--- bash_conf/002-hello.sh
+++ bash_conf/002-hello.sh
@@ -4,17 +4,25 @@ echoColor "Olá, como vai $BGreen$USER?$Color_Off Hora: $CCyan`date` :: $nixName
 
 sys_status() {
 
+    MAIN_IP=$(ip route get 8.8.8.8 | head -1 | awk '{print $7}')
+    UPTIME=$(uptime -p)
+
     RAM=$(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
     LOAD=$(uptime | awk -F'[a-z]:' '{ print $2}')
-    DISK=$(df -h | awk '$NF=="/"{printf "%d/%dGB (%s)\n", $3,$2,$5}')
+    DISK=$(df -l -h | awk '$NF=="/"{printf "%d/%dGB (%s)\n", $3,$2,$5}')
     CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+    LINUX="$(lsb_release -sd) - $(lsb_release -sc)"
 
     cat <<EOF
-    System Status:
-    $(echo "Load :")$(echo "$LOAD")
-    $(echo "CPU  :") $(echo "$CPU")
-    $(echo "RAM  :") $(echo "$RAM")
-    $(echo "Disk :") $(echo "$DISK")
+    System Status: $LINUX
+    $(echo "Load   :") $(echo "$LOAD")
+    $(echo "CPU    :") $(echo "$CPU")
+    $(echo "RAM    :") $(echo "$RAM")
+    $(echo "Disk   :") $(echo "$DISK")
+    $(echo "IP     :") $(echo "$MAIN_IP")
+    $(echo "UPTIME :") $(echo "$UPTIME")
+
+
 EOF
 
 }
