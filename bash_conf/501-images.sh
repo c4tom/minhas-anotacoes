1e790e00d8d754d3b85885490ce85d3448d943dd
Candido Tominaga <candido1212@gmail.com>
2021-03-11 11:32:15 -0300

    ct_image_quality

---
+++ bash_conf/501-images.sh
@@ -0,0 +1,15 @@
+
+
+[[ -f /usr/bin/convert ]] || { 
+	ct_convert_install() {
+		sudo apt install nmap
+	}
+	return ; 
+}
+
+ct_image_quality() {
+   : ${1?' <img path/name in'}
+   : ${2?' <img path/name out'}
+	: ${3?' <quality 1-100>'}
+   echo_and_run convert -quality "$3" "$1" "$2"
+}
