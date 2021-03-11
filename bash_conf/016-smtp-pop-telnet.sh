b13759f22e0ba7ec383b49558631da31c932f491
Candido Tominaga <candido1212@gmail.com>
2021-03-11 11:30:17 -0300

    askToPassword

--- bash_conf/016-smtp-pop-telnet.sh
+++ bash_conf/016-smtp-pop-telnet.sh
@@ -1,26 +1,23 @@
+ct_imap_test_ssl() {
+   : ${1?: '<imap servername>'}
+   : ${2?: '<email>'}
+   : ${3:-'993'}
 
+   password=$(askToPassword "Digite a senha do email ($2): ")
 
-   ct_imap_test_ssl() {
-      : ${1?: '<imap servername>'}
-      : ${2?: '<email>'}
-      : ${3:-'993'}
-      echo "Digite a senha do email:"
-      read password
-
-      openssl s_client -connect $1:993 -quiet <<EOF
-   a1 LOGIN $2 $password
-   a2 LIST "" "*"
-   a3 LOGOUT
-   EOF
-
-   }
+   openssl s_client -connect $1:993 -quiet <<EOF
+a1 LOGIN $2 $password
+a2 LIST "" "*"
+a3 LOGOUT
+EOF
+}
 
 ct_imap_test() {
    : ${1?: '<imap servername>'}
    : ${2?: '<email>'}
    : ${3:-'993'}
-   echo "Digite a senha do email:"
-   read password
+
+   password=$(askToPassword "Digite a senha do email ($2): ")
 
    telnet $1 imap <<EOF
 a1 LOGIN $2 $password
