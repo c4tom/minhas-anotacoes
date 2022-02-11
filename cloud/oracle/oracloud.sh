c3f77c04c9c3a3f9613188943e897766efc67a2c
Candido Tominaga <candido1212@gmail.com>
2022-02-11 10:31:43 -0300

    install_lsmcd

--- cloud/oracle/oracloud.sh
+++ cloud/oracle/oracloud.sh
@@ -20,18 +20,6 @@ ssh-keygen -t rsa -N "" -b 4096 -C "" -f ~/.ssh/id_rsa_oracloud ; cat ~/.ssh/id_
 
 
 
-## Firewall
-sudo apt install firewalld
-sudo firewall-cmd --zone=public --permanent --add-port=80/tcp
-sudo firewall-cmd --reload
-
-sudo firewall-cmd --zone=public --permanent --add-port=7080/tcp
-sudo firewall-cmd --reload
-
-
-sudo firewall-cmd --zone=public --permanent --add-port=3306/tcp
-sudo firewall-cmd --reload
-
 
 sudo firewall-cmd --zone=public --list-ports
 
@@ -45,19 +33,67 @@ sudo firewall-cmd --zone=public --list-ports
 # Se for para ubuntu minimal
 sudo apt-get install software-properties-common
 
+# Instalar Pacotes
+apt install firewalld mariadb-client-core-10.3 net-tools
+
+# Setando firewall
+sudo firewall-cmd --zone=public --permanent --add-port=80/tcp # http
+sudo firewall-cmd --zone=public --permanent --add-port=443/tcp # https
+sudo firewall-cmd --zone=public --permanent --add-port=7080/tcp # OLS
+sudo firewall-cmd --reload
+
+# ao reiniciar VM
+service firewalld restart
+
 sudo su - 
 
-bash <( curl -k https://raw.githubusercontent.com/litespeedtech/ols1clk/master/ols1clk.sh )
+wget https://raw.githubusercontent.com/litespeedtech/ols1clk/master/ols1clk.sh && bash ols1clk.sh
 
 
+# para obter a senha do admin /usr/local/lsws/adminpasswd
+
+# https://docs.litespeedtech.com/products/lsmcd/getting_started/
+install_lsmcd() {
+	sudo apt-get install git build-essential zlib1g-dev libexpat1-dev openssl libssl-dev libsasl2-dev libpcre3-dev -y
+	cd /tmp
+	git clone https://github.com/litespeedtech/lsmcd.git
+	cd lsmcd
+	./fixtimestamp.sh
+	./configure CFLAGS=" -O3" CXXFLAGS=" -O3"
+	make
+	sudo make install
+	sudo chown -R ubuntu /usr/local/lsmcd
+	sudo systemctl enable lsmcd
+	sudo systemctl start lsmcd
+
+	sudo apt install 
+}
+
+# https://openlitespeed.org/kb/lets-encrypt-ssl-on-openlitespeed/
+install_lets_encrypt() {
+	
+}
 
 
 
 # VM Mysql
 
 sudo apt update
-sudo apt install mysql
+sudo apt install mariadb-server firewalld vim net-tools
+
+#sudo firewall-cmd --zone=public --permanent --add-port=3306/tcp
+#sudo firewall-cmd --reload
+
+sudo firewall-cmd --new-zone=mysqlzone --permanent
+sudo firewall-cmd --reload
+sudo firewall-cmd --permanent --zone=mysqlzone --add-source=10.0.0.0/24
+sudo firewall-cmd --permanent --zone=mysqlzone --add-port=3306/tcp
+sudo firewall-cmd --reload
 
 # Adicionar usuario
 
-echo "CREATE USER 'cyberpanel'@'10.0.0.%' IDENTIFIED BY 'myr00t@pass'; GRANT ALL ON *.* TO 'cyberpanel'@'10.0.0.%' WITH GRANT OPTION; flush privileges;" | mysql
+echo "CREATE USER 'olspanel'@'localhost' IDENTIFIED BY 'myr00t@pass'; GRANT ALL ON *.* TO 'olspanel'@'localhost' WITH GRANT OPTION; flush privileges;" | mysql
+echo "CREATE USER 'olspanel'@'vm_mqaaf-ols' IDENTIFIED BY 'myr00t@pass'; GRANT ALL ON *.* TO 'olspanel'@'vm_mqaaf-ols' WITH GRANT OPTION; flush privileges;" | mysql
+
+
+sed -i "s#127.0.0.1#0.0.0.0#" /etc/mysql/mariadb.conf.d/50-server.cnf
