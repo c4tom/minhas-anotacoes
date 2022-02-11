a9100db886f21b28821779d8be6ef15e87d5b08c
Candido Tominaga <candido1212@gmail.com>
2022-02-11 10:29:15 -0300

    ct_wine_install_last

---
+++ bash_conf/101-wine.sh
@@ -0,0 +1,11 @@
+
+
+# https://wiki.winehq.org/Ubuntu
+ct_wine_install_last() {
+   sudo dpkg --add-architecture i386 
+   wget -nc https://dl.winehq.org/wine-builds/winehq.key
+   sudo apt-key add winehq.key
+   sudo apt update
+   
+   sudo apt install --install-recommends winehq-stable
+}
