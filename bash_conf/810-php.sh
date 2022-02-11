ad081ce3b51a86087ae815e0823ccc28d066327d
Candido Tominaga <candido1212@gmail.com>
2022-02-11 10:30:21 -0300

    ct_ComposerSpeedUp -> ct_php_composerSpeedUp

--- bash_conf/810-php.sh
+++ bash_conf/810-php.sh
@@ -1,11 +1,10 @@
 
 [[ ! -f /usr/bin/composer ]] || {
-    ct_ComposerSpeedUp() {
+    ct_php_composerSpeedUp() {
         sudo sh -c "echo 'precedence ::ffff:0:0/96 100' >> /etc/gai.conf"
         composer global require hirak/prestissimo
     }
-
-}
+} 
 
 [[ ! -f /desenv/php/phpcs.phar ]] || {
     alias phpcs=/desenv/php/phpcs.phar
