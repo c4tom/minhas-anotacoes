4c3ca140c4aed55bf9f089fac14d8aa41bf086f6
Candido Tominaga <candido1212@gmail.com>
2022-04-12 16:30:40 -0300

    update mymint_encrypt_hdd

--- bash_conf/999-1-my-mint.sh
+++ bash_conf/999-1-my-mint.sh
@@ -46,7 +46,29 @@ mymint_install_packages() {
     # Web Dev
     aptadd php-cli
 
+    # Control Panel
+    aptadd gnome-control-center
+
     sudo apt-get install $APT_PKG
+
+    # Criar icone
+    echo "[Desktop Entry]
+Name[pt]=Aplicações Preferidas
+Name[pt_BR]=Contas Online
+Name=Preferred Applications
+Comment[pt]=Selecione as suas aplicações predefinidas
+Comment[pt_BR]=Selecione seus aplicativos padrão
+Comment=Select your default applications
+Exec=gnome-control-center online-accounts
+Icon=preferences-desktop-default-applications
+Terminal=false
+Type=Application
+StartupNotify=true
+Categories=GTK;Settings;X-MATE-PersonalSettings;
+Keywords[pt]=mate-control-center;MATE;pré-definida;preferida;aplicações;
+Keywords=mate-control-center;MATE;default;preferred;applications;
+OnlyShowIn=MATE;
+" > ~
         
 }
 
@@ -98,7 +120,7 @@ mymint_encrypt_hdd() {
     sudo cryptsetup luksAddKey $DEVHDD /root/lukskey
 
     echo "Escrevendo em /etc/crypttab"
-    echo "secret  /dev/$DEVHDD       /root/lukskey" | sudo tee -a /etc/crypttab
+    echo "secret  $DEVHDD       /root/lukskey" | sudo tee -a /etc/crypttab
 
     cat /etc/crypttab
 }
