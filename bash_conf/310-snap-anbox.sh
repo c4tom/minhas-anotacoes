f48891145d0480b8d3fe33cfb8e36f035590c8c6
Candido Tominaga <candido1212@gmail.com>
2022-04-28 12:31:30 -0300

    Update Install Anbox to Mint 20.3

--- bash_conf/310-snap-anbox.sh
+++ bash_conf/310-snap-anbox.sh
@@ -3,37 +3,24 @@
 [[ -f /usr/bin/snap ]] || { return ; }
 
 # https://github.com/anbox/anbox/blob/master/docs/install.md
-# https://docs.anbox.io/userguide/install.html
-# https://linuxconfig.org/how-to-install-anbox-and-run-android-apps-in-linux
+# https://www.howtogeek.com/760044/how-to-run-android-apps-on-linux/
 
+# FOR Mint 20.3
 ct_anboxInstall() {
-    sudo add-apt-repository ppa:morphis/anbox-support
-    sudo apt update
-    sudo apt install anbox-modules-dkms
-
-    sudo modprobe ashmem_linux
-    sudo modprobe binder_linux
-
-
-    ls -1 /dev/{ashmem,binder}
-
     sudo snap install --devmode --beta anbox
+    sudo apt install wget curl lzip tar unzip squashfs-tools
+
+    echo "Install PLAY Store"
+    wget https://raw.githubusercontent.com/geeks-r-us/anbox-playstore-installer/master/install-playstore.sh
+    chmod +x install-playstore.sh
+    ./install-playstore.sh
 
     snap info anbox
+
+    echo "Run Now anbox.appmgr"
 }
 
-
-ct_anboxRestartService() {
-    sudo systemctl restart snap.anbox.container-manager.service
-}
-
-#https://github.com/Debyzulkarnain/anbox-bridge/blob/master/anbox-bridge.sh
-ct_anboxBridge() {
-    sudo /usr/bin/anbox-bridge.sh start
-}
 
 ct_anboxUninstall(){
     sudo snap remove anbox
-    sudo apt install ppa-purge
-    sudo ppa-purge ppa:morphis/anbox-support
 }
