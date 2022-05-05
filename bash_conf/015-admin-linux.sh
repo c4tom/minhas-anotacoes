60f2cf8e27096f05142533cfa9640c0e9ae16b3e
Candido Tominaga <candido1212@gmail.com>
2022-05-05 16:57:31 -0300

    ct_swap_create

--- bash_conf/015-admin-linux.sh
+++ bash_conf/015-admin-linux.sh
@@ -13,15 +13,38 @@ ct_adm_user_create_jailed_in_home() {
    sudo useradd -s /bin/rbash -m -d /home/$1 $1
 }
 
-ct_adm_how_process_running_port () {
-   : ${1?: '<port>'}
+
+[[ ! -f $(type -P lsof) ]] || { 
+
+   ct_adm_how_process_running_port () {
+      : ${1?: '<port>'}
 
 
-   echo_and_run sudo lsof -i :$1
+      echo_and_run sudo lsof -i :$1
 
-   local cmd="sudo netstat -ltnp | grep -w ':$1'"
-   echoColor "$BYellow""command>$IPurple $cmd"
-   "$($cmd)"
-   # TODO corrigir
+      local cmd="sudo netstat -ltnp | grep -w ':$1'"
+      echoColor "$BYellow""command>$IPurple $cmd"
+      "$($cmd)"
+      # TODO corrigir
+   }
 }
 
+
+
+ct_swap_create() {
+   local SWAP_LOCATION=${1:-"/swapfile"}
+   local SIZE_BYTES=${2:-"3145728"}
+   cat /proc/sys/vm/swappiness
+   sudo sysctl vm.swappiness=10
+   sudo dd if=/dev/zero of=$SWAP_LOCATION bs=1024 count=$SIZE_BYTES
+   sudo chmod 600 $SWAP_LOCATION
+
+   sudo mkswap $SWAP_LOCATION
+   sudo swapon $SWAP_LOCATION
+
+   echo "$SWAP_LOCATION swap swap defaults 0 0" | sudo tee --append /etc/fstab
+
+   sudo swapon --show
+
+   sudo free -h
+}
