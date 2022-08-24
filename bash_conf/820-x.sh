6ec3ed49f61f6d591f3a3d78ee596bb4e23801cd
Candido Tominaga <candido1212@gmail.com>
2022-08-23 21:15:14 -0300

    ct_x_prepare_to_display() { ct_x_export_display_as_root

---
+++ bash_conf/820-x.sh
@@ -0,0 +1,17 @@
+
+
+ct_x_prepare_to_display() {
+    xauth list $DISPLAY > /tmp/.x
+}
+
+ct_x_export_display_as_root() {
+    isRoot
+
+    if [[ -f /tmp/.x ]] 
+    then
+        xauth add $(cat /tmp/.x)
+        export DISPLAY=localhost:10.0
+    else
+        echo "arquivo /tmp/.x not found. Please, run as user ct_x_prepare_to_display"
+    fi
+}
