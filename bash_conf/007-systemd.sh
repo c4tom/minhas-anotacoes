be90f30e96246808876f5393c7736f1beb91cc91
Candido Tominaga <candido1212@gmail.com>
2022-09-06 15:07:50 -0300

    alias ct_cockpit_start ct_cockpit_stop

--- bash_conf/007-systemd.sh
+++ bash_conf/007-systemd.sh
@@ -92,7 +92,10 @@ ct_systemd_cockpit_start() {
     echo_and_run openAnyFile http://localhost:9090
 }
 
+alias ct_cockpit_start=ct_systemd_cockpit_start
+
 ct_systemd_cockpit_stop() {
     echo_and_run sudo systemctl stop cockpit.socket
     echo_and_run sudo systemctl disable cockpit.socket
 }
+alias ct_cockpit_stop=ct_systemd_cockpit_stop
