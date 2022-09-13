300f6f4c39f68ae0579e321ab4c6393dd894847c
Candido Tominaga <candido1212@gmail.com>
2022-09-13 09:01:35 -0300

    renomeado e adicionados algumas funções

--- bash_conf/920-oraclecloud-vm.sh
+++ bash_conf/920-oraclecloud-vm.sh
@@ -7,7 +7,7 @@ ct_isRoot() {
     [[ $(id -u) -eq 0 ]] || { echo >&2 "Must be root to run this function"; kill -INT $$; }
 }
 
-ct_oracloud_install_pacotes() {
+ct_oracloud_install_utils_packages() {
    sudo apt install iputils-ping inetutils-telnet firewalld net-tools bash-completion
 }
 
@@ -26,13 +26,13 @@ ct_oracloud_bash_conf() {
 }
 
 
-ct_oracloud_mariadbServer() {
+ct_oracloud_install_mariadbServer() {
    sudo apt update
    sudo apt install mariadb-server
 }
 
-ct_oracle_firewalld() {
-   : ${1?' <list ports - enclosed by quotes>'}
+ct_oracloud_firewalld_add_rules() {
+   : ${1?' <list ports - enclosed by quotes. "80 443 3389 3306">'}
    : ${2?' zone=(public|docker|????). Ps: get all active zone-> firewall-cmd --get-active-zones'}
 
    for i in $1
@@ -43,19 +43,40 @@ ct_oracle_firewalld() {
    echo_and_run sudo firewall-cmd --zone=$2 --list-ports
 }
 
-ct_oracle_firewalld_listAll() {
+ct_oracloud_firewalld_start_enable_service() {
+   sudo systemctl start firewalld
+   sudo systemctl enable firewalld
+}
+
+ct_oracloud_firewalld_listAll() {
    echo_and_run sudo firewall-cmd --list-all
 }
 
-ct_oracle_firewalld_getServices() {
+# Get all services available
+ct_oracloud_firewalld_getServices() {
    echo_and_run sudo firewall-cmd --get-services
 }
 
-ct_oracle_install_ols() {
+# install OpenLiteSpeed
+ct_oracloud_install_ols() {
    cd /tmp
    wget -O - https://repo.litespeed.sh | sudo bash
    sudo apt update
    sudo apt-get install openlitespeed
    sudo apt-get install lsphp81 lsphp81-common lsphp81-curl lsphp81-mysql lsphp81-opcache lsphp81-imap lsphp81-opcache 
 
+}
+
+# install log/report newRelic for monitoring PHP
+ct_oracloud_install_newrelicPHP() {
+   echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' | sudo tee /etc/apt/sources.list.d/newrelic.list
+   wget -O- https://download.newrelic.com/548C16BF.gpg | sudo apt-key add -
+   sudo apt-get update
+   sudo apt-get install newrelic-php5 -y
+   sudo newrelic-install install
+
+   sudo NR_INSTALL_SILENT=1 newrelic-install install
+
+   #systemctl restart lsws
+   #killall lsphp
 }
