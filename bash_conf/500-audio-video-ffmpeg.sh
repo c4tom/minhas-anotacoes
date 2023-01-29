5ce248f0171a1bb91cb6b3e4b9d5c5b120da923e
Candido Tominaga <candido1212@gmail.com>
2023-01-28 23:44:32 -0300

    ct_ffmpeg_convertToOdysee

--- bash_conf/500-audio-video-ffmpeg.sh
+++ bash_conf/500-audio-video-ffmpeg.sh
@@ -57,8 +57,9 @@ ct_video_isValid() {
 
 
 
-
-
+ct_ffmpeg_convertToOdysee() {
+  ffmpeg -i "$1" -c:v libx264 -crf 21 -preset faster -pix_fmt yuv420p -maxrate 5000K -bufsize 5000K -vf 'scale=if(gte(iw\,ih)\,min(1920\,iw)\,-2):if(lt(iw\,ih)\,min(1920\,ih)\,-2)' -movflags +faststart -c:a aac -b:a 128k "$2"
+}
 
 
 
