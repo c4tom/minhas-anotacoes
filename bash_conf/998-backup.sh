4c7428fc045cc7e9e0089aaabf163ed5912c3fba
Candido Tominaga <candido1212@gmail.com>
2023-02-27 22:13:00 -0300

    ct_backupMyHomeWithEncrypted atualizado

--- bash_conf/998-backup.sh
+++ bash_conf/998-backup.sh
@@ -7,6 +7,15 @@ ct_backupMyHomeWithEncrypted() {
     echo_and_run tar -cv --use-compress-program=pigz -f backup-$USER-$DATE.tar.gz \
     --exclude=backup-name.tar.gz\
     --exclude=.cache \
+    --exclude=.config/Code/Cache* \
+    --exclude=.config/Code/logs \
+    --exclude=.config/Code/Crashpad \
+    --exclude=".config/Code/Service Worker/CacheStorage" \
+    --exclude=".config/google-chrome/Crash Reports" \
+    --exclude="*/CacheStorage/*" \
+    --exclude="*/Cache/*" \
+    --exclude="*/cache/*" \
+    --exclude="*/Code Cache/*" \
     --exclude=.debug \
     --exclude=.dbus \
     --exclude=.gvfs \
@@ -23,5 +32,6 @@ ct_backupMyHomeWithEncrypted() {
     --exclude=.npm \
     --exclude=".local/share/NuGet/v3-cache" \
     --exclude=".wine/drive_c/users/can/Application Data/VOS" \
+    --exclude="VirtualBox VMs" \
     --exclude=Templates /home/$USER
 }
