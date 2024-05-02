189d0050c6a1501653770cf5cfec039e38a32063
Candido Tominaga <candido1212@gmail.com>
2024-05-01 23:24:51 -0300

    ct_pgadmin4_install

--- bash_conf/131-pgsql.sh
+++ bash_conf/131-pgsql.sh
@@ -1,1 +1,33 @@
 # https://www.tecmint.com/mysql-mariadb-postgresql-database-backup-using-automysqlbackup-autopostgresqlbackup/
+
+ct_pgadmin4_install() {
+    #
+    # Setup the repository
+    #
+    ubuntu_codename=jammy
+
+
+    # Install the public key for the repository (if not done previously):
+    curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+
+    # Create the repository configuration file:
+    sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$ubuntu_codename pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
+    
+    sudo apt update;
+
+    #
+    # Install pgAdmin
+    #
+
+    # Install for both desktop and web modes:
+    sudo apt install pgadmin4
+
+    # Install for desktop mode only:
+    sudo apt install pgadmin4-desktop
+
+    # Install for web mode only: 
+    sudo apt install pgadmin4-web 
+
+    # Configure the webserver, if you installed pgadmin4-web:
+    sudo /usr/pgadmin4/bin/setup-web.sh
+}
