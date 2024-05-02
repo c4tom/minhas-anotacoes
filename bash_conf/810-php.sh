0fa7000f04515d647381c03656b3b6200efd9cac
Candido Tominaga <candido1212@gmail.com>
2024-05-01 23:25:15 -0300

    php_composer_install

--- bash_conf/810-php.sh
+++ bash_conf/810-php.sh
@@ -1,11 +1,26 @@
-
 [[ ! -f /usr/bin/composer ]] || {
     ct_php_composerSpeedUp() {
+        # Melhora a resolução de endereços IPv4 em ambientes que usam IPv6
         sudo sh -c "echo 'precedence ::ffff:0:0/96 100' >> /etc/gai.conf"
+
+        # Instala o plugin Prestissimo globalmente usando o Composer
         composer global require hirak/prestissimo
     }
-} 
+
+}
 
 [[ ! -f /desenv/php/phpcs.phar ]] || {
     alias phpcs=/desenv/php/phpcs.phar
+}
+
+[[ -f /usr/bin/composer ]] || {
+    php_composer_install() {
+        # Verifica se o Composer já está instalado
+        # Baixa e instala o Composer
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php composer-setup.php --install-dir=/usr/bin --filename=composer
+        php -r "unlink('composer-setup.php');"
+
+    }
+    return
 }
