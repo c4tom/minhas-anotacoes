710c57ab2d5d12be905fd972986c7bc456012b07
Candido Tominaga <candido1212@gmail.com>
2024-07-16 10:55:30 -0300

    desabilitado PS1= por causa do zsh

--- bash_conf/200-git.sh
+++ bash_conf/200-git.sh
@@ -10,7 +10,7 @@ _gitParseBranch() {
 
 # mostra branch atual
 #export PS1="\n\w $(_gitParseBranch)\n${CCyan}\u@\h${NC} >${NC}"
-export PS1="$CCyan\u@\h>\[\033[32m\]\w$BIYellow\$(_gitParseBranch)\[\033[00m\]>"
+#export PS1="$CCyan\u@\h>\[\033[32m\]\w$BIYellow\$(_gitParseBranch)\[\033[00m\]>"
 #export PS1="\$(_gitParseBranch)\n\u@\h \w >"
 #export PS1="\u@\h\n\w\$(_gitParseBranch)>"
 
